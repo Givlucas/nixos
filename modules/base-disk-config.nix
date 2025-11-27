40ac59e6193ebf304bd4543628d44d3f4e573461
Lucas Givens <lucas.givens@proton.me>
2025-11-27 13:34:07 -0500

    updated basic disko to have options for target drive

--- modules/base-disk-config.nix
+++ modules/base-disk-config.nix
@@ -1,27 +1,36 @@
-{ config, ... }:
+{ config, lib ,... }:
 {
-  boot.loader.grub.devices = [ config.disko.devices.disk.sda.device ];
-  disko.devices.disk.main ={
-    device = "/dev/sda";
-    type = "disk";
-    content = {
-      type = "gpt";
-      partitions = {
-        ESP = {
-          type = "EF00";
-          size = "500M";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
+
+  options.base-disk = {
+    device = lib.mkOption {
+      type = lib.types.str;
+    };
+  };
+
+  config = {
+    boot.loader.grub.devices = [ config.disko.devices.disk.main.device ];
+    disko.devices.disk.main = {
+      device = config.base-disk.device;
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            type = "EF00";
+            size = "500M";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
           };
-        };
-        root = {
-          size = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
           };
         };
       };
