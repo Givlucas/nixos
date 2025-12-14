5e4381968c15515dbfb29270d81864b69a951e12
Lucas Givens <givlucas@gmail.com>
2025-12-14 14:41:59 +0000

    added configuration for automatic timezone

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -13,6 +13,12 @@ in
       default = true;
       description = "Enable edge tiling (snap windows to edges)";
     };
+
+    automaticTimezone = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable automatic timezone detection";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -24,16 +30,24 @@ in
     # Edge tiling via mutter GSettings
     services.xserver.desktopManager.gnome.extraGSettingsOverridePackages =
       lib.mkIf cfg.edgeTiling [ pkgs.mutter ];
-    services.xserver.desktopManager.gnome.extraGSettingsOverrides =
-      lib.mkIf cfg.edgeTiling ''
+    services.xserver.desktopManager.gnome.extraGSettingsOverrides = ''
+      ${lib.optionalString cfg.edgeTiling ''
         [org.gnome.mutter]
         edge-tiling = true
-      '';
+      ''}
+      ${lib.optionalString cfg.automaticTimezone ''
+        [org.gnome.desktop.datetime]
+        automatic-timezone = true
+      ''}
+    '';
 
     # GNOME packages (gnome-tweaks moved to per-user packages)
     environment.systemPackages = [ ];
 
     # Disable PulseAudio (use PipeWire via GNOME)
     hardware.pulseaudio.enable = false;
+
+    # Enable automatic timezone detection
+    services.automatic-timezoned.enable = true;
   };
 }
